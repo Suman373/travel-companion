7bdb3cbb8da25c567ab2d7341e8e06d7784a6bc7
Suman <iamsuman898@gmail.com>
2022-08-22 18:36:36 +0530

    debug

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,43 +1,29 @@
 import React from "react";
 import '../styles/Banner.css';
 import travelbanner from '../assets/travelbanner.png';
-
 import { useNavigate } from "react-router-dom";
-
-
-const Banner = () => {
-
-        const navigate = useNavigate();
-    
-    const handleGetstarted=(e)=>{
-        e.preventDefault();
-        navigate('/user');
-
 import {AiOutlineArrowRight} from 'react-icons/ai';
-import { useNavigate } from "react-router-dom";
+
 
 const Banner=()=>{
     // navigator
     const navigate = useNavigate();
 
-    const handleRedirect=(e)=>{
-        navigate('/register');
-
+     
+    const handleGetstarted=(e)=>{
+        e.preventDefault();
+        navigate('/user');
     }
-
+    
     return(
         <>
         <div className="banner-container">
 
             <div className="title-container">
                     <h1>Travel Companion</h1>
-
-                    <h5>A new journey awaits !</h5>
-                    <button onClick={(e)=>handleGetstarted(e)}>Get started</button>
-
                     <h5>It's a big world out there, Go explore .</h5>
                     <button 
-                    onClick={(e=> handleRedirect(e))}>
+                    onClick={(e)=> handleGetstarted(e)}>
                         Get started <AiOutlineArrowRight></AiOutlineArrowRight>
                     </button>
 
