b0573891292c4297f69ec70361e297027f2ed605
Jay <mkvjayanth1@gmail.com>
2022-08-23 14:56:38 +0530

    merge a

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import '../styles/Banner.css';
 import travelbanner from '../assets/travelbanner.png';
-import { useNavigate } from "react-router-dom";
 import {AiOutlineArrowRight} from 'react-icons/ai';
-
+import { useNavigate } from "react-router-dom";
 
 const Banner=()=>{
     // navigator
     const navigate = useNavigate();
 
-     
-    const handleGetstarted=(e)=>{
-        e.preventDefault();
-        navigate('/user');
+    const handleRedirect = (e) => {
+        navigate('/register');
     }
-    
+
     return(
         <>
         <div className="banner-container">
 
             <div className="title-container">
                     <h1>Travel Companion</h1>
+
+                    <h5>A new journey awaits !</h5>
+
                     <h5>It's a big world out there, Go explore .</h5>
                     <button 
-                    onClick={(e)=> handleGetstarted(e)}>
+                    onClick={(e=> handleRedirect(e))}>
                         Get started <AiOutlineArrowRight></AiOutlineArrowRight>
                     </button>
 
